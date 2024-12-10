d9b7a3f2407a40e4f6178b60c804c2f7d58a6e48
CVG42 <zap653@amerike.edu.mx>
2024-12-09 17:44:16 -0800

    Flip towards mouse position

--- Assets/Scripts/PLAYER/PlayerController.cs
+++ Assets/Scripts/PLAYER/PlayerController.cs
@@ -6,8 +6,12 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
+    [SerializeField] Animator animator;
+    [SerializeField] bool isFlipped;
+
     Rigidbody2D rb;
     Vector2 movementInput;
+    Vector2 mousePos;
 
     void Start()
     {
@@ -17,10 +21,36 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         rb.velocity = movementInput * moveSpeed;
+        LookPosition();
     }
 
     public void Move(InputAction.CallbackContext context)
     {
+        animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+            animator.SetBool("isWalking", false);
+
+        if (!isFlipped)
+            Flip(movementInput.x);
+
         movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+    }
+
+    private void LookPosition()
+    {
+        if (isFlipped)
+            Flip(movementInput.x);
+    }
+
+    private void Flip(float x)
+    {
+        if (x != 0)
+            transform.localScale = new Vector3(Mathf.Sign(x), 1, 1);
     }
 }
